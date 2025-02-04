79c3a5a90d392f3dfaf96709a8d76d76d79aece5
VishnuShreeKrishna <Vishnusharma5615@gmail.com>
2025-02-04 10:04:29 +0530

    commit

--- script.js
+++
@@ -1,7 +0,0 @@
-// Navigation effect
-document.querySelectorAll("nav ul li a").forEach(link => {
-  link.addEventListener("click", () => {
-      document.querySelectorAll("nav ul li a").forEach(link => link.classList.remove("active"));
-      link.classList.add("active");
-  });
-});
